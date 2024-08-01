c37acb13a366209c686f7991054c1058609f842c
RalphGodCreative <ralphgodtpe@gmail.com>
2024-08-01 22:51:36 +0800

    optimize backend for transaction

--- src/main/java/RGcards/SportsCardProject/controller/TransactionController.java
+++ src/main/java/RGcards/SportsCardProject/controller/TransactionController.java
@@ -1,2 +1,29 @@
-package RGcards.SportsCardProject.controller;public class TransactionController {
+package RGcards.SportsCardProject.controller;
+
+import RGcards.SportsCardProject.component.CardComponent;
+import RGcards.SportsCardProject.eto.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@RequestMapping("/transaction")
+@Controller
+@ComponentScan
+public class TransactionController {
+
+    @Autowired
+    private CardComponent cardComponent;
+
+    @GetMapping("/")
+    public String allTransaction(Model model){
+        List<Transaction> transactions = cardComponent.findAllTransactionsSortByDate();
+        model.addAttribute("transactions" , transactions);
+
+        return "";
+    }
 }
