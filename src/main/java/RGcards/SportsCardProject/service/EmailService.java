9330746fd7858fcff0a41be8a5ceea59709d71a7
Ralph Laptop <ralphgodtpe@gmail.com>
2025-10-18 15:03:51 +0800

    refactor: improve EmailService structure and add documentation for email methods

--- src/main/java/RGcards/SportsCardProject/service/EmailService.java
+++ src/main/java/RGcards/SportsCardProject/service/EmailService.java
@@ -5,7 +5,6 @@ import RGcards.SportsCardProject.entity.SearchProduct;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -26,17 +25,21 @@ import java.util.Map;
 public class EmailService {
 
     private final Environment env;
+    private final JavaMailSender mailSender;
+    private final SpringTemplateEngine springTemplateEngine;
 
-    @Autowired
-    private JavaMailSender mailSender;
-
-    @Autowired
-    private SpringTemplateEngine springTemplateEngine;
-
-    public EmailService(Environment env) {
+    public EmailService(Environment env, JavaMailSender mailSender, SpringTemplateEngine springTemplateEngine) {
         this.env = env;
+        this.mailSender = mailSender;
+        this.springTemplateEngine = springTemplateEngine;
     }
 
+    /**
+     * Sends an email with search results using an HTML template.
+     *
+     * @param resultList Map of SearchKeyword to list of SearchProduct results
+     * @throws MessagingException if sending the email fails
+     */
     public void sendSearchResultEmail(Map<SearchKeyword, List<SearchProduct>> resultList) throws MessagingException {
         Map<String, Object> variable = new HashMap<>();
         variable.put("resultList", resultList);
@@ -47,13 +50,26 @@ public class EmailService {
         sendHtmlEmail(env.getProperty("app.mail.to"), "Yahoo Auction Search Result " + formattedDate, mail);
     }
 
+    /**
+     * Builds the email content from a Thymeleaf template and variables.
+     *
+     * @param templateName the name of the Thymeleaf template
+     * @param variables    the variables to populate the template
+     * @return the processed email content as a String
+     */
     public String buildEmailContent(String templateName, Map<String, Object> variables) {
         Context context = new Context();
         context.setVariables(variables);
         return springTemplateEngine.process(templateName, context);
     }
 
-
+    /**
+     * Sends a simple text email.
+     *
+     * @param to      recipient email address
+     * @param subject email subject
+     * @param text    email body text
+     */
     public void sendSimpleEmail(String to, String subject, String text) {
         SimpleMailMessage message = new SimpleMailMessage();
         message.setTo(to);
@@ -63,9 +79,15 @@ public class EmailService {
         mailSender.send(message);
     }
 
+    /**
+     * Sends an HTML email asynchronously.
+     *
+     * @param to          recipient email address
+     * @param subject     email subject
+     * @param htmlContent HTML content of the email
+     */
     @Async
-    public void sendHtmlEmail(String to, String subject, String htmlContent) throws MessagingException {
-
+    public void sendHtmlEmail(String to, String subject, String htmlContent) {
         try {
             MimeMessage message = mailSender.createMimeMessage();
             MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
@@ -73,14 +95,12 @@ public class EmailService {
             helper.setTo(to);
             helper.setSubject(subject);
             helper.setText(htmlContent, true);
-//            System.out.println(htmlContent);
+
             log.info("sending email {} to {}", subject, to);
 
             mailSender.send(message);
-        } catch (Exception e) {
-            log.error("encounter error sending email");
-            e.printStackTrace();
+        } catch (MessagingException e) {
+            log.error("Encountered error sending email", e);
         }
-
     }
 }
